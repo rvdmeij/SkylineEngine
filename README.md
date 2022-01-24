4dc96c97055cc8a0eecc38007e4b441d2e90016a
Wesley <wjapajoe@ziggo.nl>
2022-01-24 03:45:10 -0300

    Changed readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # SkylineEngine
 Unity inspired game engine written in C#
+
+# Dependencies
+https://www.nuget.org/packages/OpenTK/4.0.0
+https://www.nuget.org/packages/AssimpNet/4.1.0
+https://www.nuget.org/packages/BulletSharp.NetCore/
+https://github.com/japajoe/imgui
+
+```csharp
+using SkylineEngine;
+
+namespace SkylineEngineApplication
+{
+    class Program
+    {
+        private static Application application;
+        private static GameObject gameObject;
+
+        static void Main(string[] args)
+        {
+            application = new Application("Skyline Engine", 512, 512, 3, 3, false);
+            application.created += OnCreated;
+            application.Run();            
+        }
+
+        static void OnCreated()
+        {
+            gameObject = new GameObject();
+            gameObject.AddComponent<GameManager>();
+        }
+    }
+
+    public class GameManager : MonoBehaviour
+    {
+        private Camera camera;
+        private GameObject cube;
+
+        void Start()
+        {
+            camera = Camera.main;            
+            camera.farClipPlane = 10000.0f;
+            camera.transform.position = new Vector3(0, 0, 0);
+
+            var firstPerson = camera.gameObject.AddComponent<FirstPersonController>();
+            firstPerson.speed *= 50;
+            firstPerson.zoomSpeed *= 200;
+
+            Light.main.strength = 1.0f;
+            Light.main.transform.position = new Vector3(0, 1000, -500);
+            Light.main.transform.rotation = new Quaternion(-0.26127627f,-0.00029115385f,0.26208368f,-0.9291085f);
+        }
+
+        //Only call ImGui/GUI functions from void OnGUI
+        void OnGUI()
+        {
+            GUI.Label(new Rect(5,5,200,20), "Skyline Engine");
+        }
+    }
+}
+```
--- SkylineEngine/Resources.cs
+++ SkylineEngine/Resources.cs
@@ -5,42 +5,7 @@ namespace SkylineEngine
 {
     public static class Resources
     {
-        public static void LoadAll<T>()
-        {
-            if(typeof(T) == typeof(Texture))
-            {
-                TextureManager.Load("res/Textures/Beach.jpg");
-                TextureManager.Load("res/Textures/Grass.jpg");
-                TextureManager.Load("res/Textures/Grass3.jpg");
-                TextureManager.Load("res/Textures/GrassFlowers.png");
-                TextureManager.Load("res/Textures/Default.png");
-                TextureManager.Load("res/Textures/Mud.png");
-                TextureManager.Load("res/Textures/SplatMap.jpg");
-                //TextureManager.Load("res/Textures/Road.jpg");
-                //TextureManager.Load("res/Textures/Road2.jpg");
-                TextureManager.Load("res/Textures/Box.jpg");
-                //TextureManager.Load("res/Textures/Moon.bmp");
-                //TextureManager.Load("res/Textures/Water6.png");
-                TextureManager.Load("res/Textures/Sun.jpg");
-                //TextureManager.Load("res/Textures/Trees/PalmTree1.png");
-                //TextureManager.Load("res/Textures/Trees/PalmTree2.png");
-                //TextureManager.Load("res/Textures/Trees/PalmTree3.png");
-                //TextureManager.Load("res/Textures/Trees/PalmTree4.png");
-                //TextureManager.Load("res/Textures/Trees/Tree1.png");
-                //TextureManager.Load("res/Textures/Trees/Tree2.png");
-                //TextureManager.Load("res/Textures/Trees/Bush1.png");
-                //TextureManager.Load("res/Textures/Trees/RedFlower.png");
-                //TextureManager.Load("res/Textures/Trees/YellowFlower.png");
-            }
-            else if(typeof(T) == typeof(Shader))
-            {
-                ShaderManager.Load("res/Shaders/Skybox.shader");
-                ShaderManager.Load("res/Shaders/Terrain.shader");
-                ShaderManager.Load("res/Shaders/Default.shader");
-                ShaderManager.Load("res/Shaders/Particle.shader");
-                return;
-            }
-        }
+
 
         public static void LoadAll<T>(List<string> resources)
         {
